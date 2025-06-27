01fd6864f0d847dc4a0f8b2d844975192120fd22
NoteAnywhere <noteanywhere@example.com>
2025-06-27 20:53:05 +0900

    feat: add cors origin url

--- src/main/java/com/garliccastle/demo/config/SecurityConfig.java
+++ src/main/java/com/garliccastle/demo/config/SecurityConfig.java
@@ -7,21 +7,24 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
+public class SecurityConfig {
 
     // Spring Security 전용 CORS 설정
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://us-hackathon-3db73.firebaseapp.com", "https://us-hackathon-3db73.firebaseapp.com", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "https://us-hackathon-3db73.firebaseapp.com",
+                "https://us-hackathon-3db73.web.app/",
+                "http://localhost:5173"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true); // 반드시 true여야 쿠키 허용
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true); // 쿠키 전송을 위한 설정
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
