58a2b5ee0bc034426d6e387f6ee81802b08bd03f
YeongJae0114 <yjl8628@naver.com>
2025-06-28 09:25:44 +0900

    시큐리티 비활성화 및 의존성 수정

--- src/main/java/com/garliccastle/demo/domain/TestController.java
+++ src/main/java/com/garliccastle/demo/domain/TestController.java
@@ -1,12 +1,20 @@
 package com.garliccastle.demo.domain;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test2")
+    @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+    @RequestMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+        // 아무것도 하지 않음
     }
 }
